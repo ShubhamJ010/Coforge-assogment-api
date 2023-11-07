2d0e356d94c88e4b0bfb61edabff035b5ed64caf
Shubham <samxn81@gmail.com>
2023-11-08 03:45:35 +0530

    DOne not sure about one to one mapping

--- src/main/java/com/example/ExamPortal/Model/User.java
+++ src/main/java/com/example/ExamPortal/Model/User.java
@@ -1,11 +1,7 @@
 package com.example.ExamPortal.Model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Data;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Data
 @Entity
